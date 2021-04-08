42c5202fff4aa1b51737fc0018ab82b7e56ca447
Joel Eckerson <joelaeckerson@gmail.com>
2021-04-07 19:43:54 -0500

    generator functions

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log('Hello from JS');
+
+function* countdown(){
+	let a = 10;
+	while( a > 0 ){
+		yield 'launching in ' + a;
+		a--;
+	}
+	yield 'blast off!';
+}
+const countdowner = countdown();
+
+function* switcher(){
+	while( true ){
+		yield 'on';
+		yield 'off';
+	}
+}
+const switcheroo = switcher();
+
+function* pageLoad(){
+	let counter = 0;
+	while( true ){   
+		if( counter > 0 ){
+			yield `you have refreshed ${ counter } times`;
+		}
+		else{
+			yield `Hello world!`;
+		}
+		counter ++;
+	}
+}
+const loader = pageLoad();
